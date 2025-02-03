2312ff8d1fac236329c1228db60b87e4b5675b60
federicodececco <federicodececco1@gmail.com>
2025-02-03 13:25:35 +0100

    add: adding elemnts to list

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
 import { useState } from 'react'
+import animals from './animals'
+import { use } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [animalName, setAnimal] = useState('')
+  const [animalsList, setAnimalList] = useState(animals)
 
-  return <></>
+  const handleSubmit = e => {
+    e.preventDefault()
+    setAnimalList(currentState => [
+      ...currentState,
+      { id: currentState[currentState.length - 1].id + 1, name: animalName },
+    ])
+
+    setAnimal('')
+  }
+  return (
+    <>
+      <div>
+        <ul>
+          {animalsList.map(elem => {
+            return <li key={elem.id}>{elem.name}</li>
+          })}
+        </ul>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            name='name'
+            type='text'
+            value={animalName}
+            onChange={e => {
+              setAnimal(e.target.value)
+            }}
+          />
+          <button type='submit'> Aggiungi </button>
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default App
